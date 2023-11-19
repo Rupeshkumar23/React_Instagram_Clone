2ee0a3d3aea1f7d82d8487ab41b90d842e328b55
Rupeshkumar23 <coolrupesh2000@gmail.com>
2023-11-19 13:20:29 +0530

    updated code

--- src/authenticate/GetappSignIn.js
+++ src/authenticate/GetappSignIn.js
@@ -4,23 +4,29 @@ import windows from "../Imgs/Micro.png";
 import playstore from "../Imgs/play.png";
 
 const GetappSignIn = () => {
+  const playstoreUrl ='https://tinyurl.com/56ms2d9v'
+  const Microsoft='https://tinyurl.com/2t8fc4xv'
   return (
     <div>
       <div className="loginPage_downloadSection">
         <div>Get the app.</div>
         <div className="loginPage_option">
-          <img
-            className="loginPage_dwimg play"
-            src={playstore}
-            width="136px"
-            alt="play"
-          />
+        <a href={playstoreUrl} target="_blank" rel="noopener noreferrer">
+      <img
+        className="loginPage_dwimg play"
+        src={playstore}
+        width="136px"
+        alt="play"
+      />
+    </a>
+    <a href={Microsoft} target="_blank" rel="noopener noreferrer">
           <img
             className="loginPage_dwimg play"
             src={windows}
             width="136px"
             alt="app"
           />
+          </a>
         </div>
       </div>
     </div>
