3fd50cc1af072cd835b534d0a1c7c219e449a7e4
Rupeshkumar23 <coolrupesh2000@gmail.com>
2023-11-29 00:30:36 +0530

    updated code

---
+++ src/navigation/data.js
@@ -0,0 +1,51 @@
+import Img_4 from "../Imgs/V_J.png";
+import Img_7 from "../Imgs/Rockstar_Games.png";
+import Img_8 from "../Imgs/Sun_news.png";
+import Img_9 from "../Imgs/gowsami.png";
+import Img_10 from "../Imgs/Tech_G.jpg";
+import tick from "../Imgs/icons8-instagram-verified.svg";
+
+
+export const searchData =[
+    {
+        id: 1,
+        user: "rockstargames",
+        userAvatar: Img_7,
+        description:"Rockstar Games-27.1M followers",
+        timestamp: "2d",
+        tickmark: tick,
+      },
+      {
+        id: 2,
+        user: "actorvijay",
+        userAvatar: Img_4,
+        description: "Vijay-9.8M followers",
+        timestamp: "1d",
+        tickmark: tick,
+      },
+      {
+        id: 3,
+        user: "sunnews",
+        userAvatar: Img_8,
+        description: "Sun News Tamil-2.6M followers",
+        timestamp: "5d",
+        tickmark: tick,
+      },
+      {
+        id: 4,
+        user: "gowsami.dev",
+        userAvatar: Img_9,
+        description:"Nina | Coder| Tech-151K followers",
+        timestamp: "6d",
+        tickmark: false,
+      },
+      {
+        id: 5,
+        user: "Techie Programmer",
+        userAvatar: Img_10,
+        description:"Nilesh | Techie Programmer-604K followers",
+        timestamp: "5d",
+        tickmark: false,
+      },
+
+];
